abab5d2a32ad96f27bec86bd9fe3bd18eea2346c
adampagot <adampagot@gmail.com>
2025-09-27 11:31:32 -0400

    added hello 11691

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("hello", "11691");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
