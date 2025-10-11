e0b253f7de6fa8d15412ff6055262fe45368bc45
adampagot <adampagot@gmail.com>
2025-10-11 17:14:54 -0400

    autontune file added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTune.java
@@ -0,0 +1,21 @@
+/*package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "AutonTune", group = "Autons")
+public class AutonTune extends AutonBase {
+    @Override
+    public void runOpMode(){
+
+        initialize();
+
+        while (opModeInInit()) {
+        }
+
+        waitForStart();
+
+        //imuDrive(.1, 5, 0);
+        //encoderStrafe(0.1,-11.75,5);
+        //imuTurn(0.1, 90);
+    }
+}*/
