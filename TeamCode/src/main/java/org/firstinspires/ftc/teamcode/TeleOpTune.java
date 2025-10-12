2b738416fbab38f559a895cd2a1ae5f1827cae46
adampagot <adampagot@gmail.com>
2025-10-12 07:21:25 -0400

    add tile for teleop tuning and testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTune.java
@@ -0,0 +1,27 @@
+/*package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.RobotControlMechanum;
+
+@TeleOp
+public class TeleOpTune extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+        RobotHardwareMap theHardwareMap = new RobotHardwareMap(this.hardwareMap, this);
+        theHardwareMap.initialize();
+
+        RobotControlMechanum robotDrive = new RobotControlMechanum(theHardwareMap, this);
+        robotDrive.initialize();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            //put test code here
+        }
+
+    }
+}*/
