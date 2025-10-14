36eb3ce09843ce0feb86eb8ebee9bbc792db9ae3
THEGOOODERDER <Aaron.Doolittle@stu.lok12.org>
2025-10-14 19:14:06 -0400

    new auton code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonredloading.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auton Sample", group = "Autons")
+public class Autonredloading extends AutonBase {
+
+    @Override
+    public void runOpMode() {
+
+        initialize();
+        waitForStart();
+        //start with only rear right corner touching wall
+        //aim at goal from smaller shooting zone
+        //activate outake before shooting
+        encoderStrafe(.5,5,3);
+        imuDrive(.5,-3,0);
+        //shoot
+        //activate intake
+        //get artifacts
+        imuDrive(.75,12,0);
+        imuTurn(.5,135);
+        //encoderStrafe(.5,-10,5);
+        //collect artifacts
+        encoderStrafe(.75,23,9);
+        imuTurn(.5,135);
+        //shoot
+
+    }
+}
