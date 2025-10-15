9a2fac0cffef2651f281da01bf7c5fd0b8f1726c
ftc <scripps-ftc@no-reply.net>
2025-10-14 20:34:51 -0400

    started initial java intake file and sigma

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.RobotHardwareMap;
+
+public class Intake {
+
+    //RobotHardwareMap robotHardwareMap;
+    private DcMotor intakeMotor;
+
+    private int speed;
+    private char direction;
+    public Intake; // finish contstructor
+
+    // finish the init method
+//    public void initialize(){
+       //robotHardwareMap.intakeMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
+    // }
+
+
+  // define set speed method
+//            robotHardwareMap.intakeMotorFront.setPower(speeds[0]);
+
+
+}
+
+
