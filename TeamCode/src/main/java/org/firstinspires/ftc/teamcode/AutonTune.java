1b61cdf285c7b8a63053a0e6d32f6ba108966513
chronicleglados <thereallferrari@gmail.com>
2025-10-14 20:37:21 -0400

    fixed the robot not spinning when we try to turn. also fixed the inverted movement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTune.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTune.java
@@ -1,4 +1,4 @@
-/*package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
@@ -16,6 +16,6 @@ public class AutonTune extends AutonBase {
 
         //imuDrive(.1, 5, 0);
         //encoderStrafe(0.1,-11.75,5);
-        //imuTurn(0.1, 90);
+        imuTurn(0.1, 90);
     }
-}*/
+}
