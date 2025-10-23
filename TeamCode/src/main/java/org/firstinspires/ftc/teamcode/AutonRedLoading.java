9731a2df67c0fa18e0a46eb389fa964e8cea4015
THEGOOODERDER <Aaron.Doolittle@stu.lok12.org>
2025-10-23 19:20:03 -0400

    Fix: Auton Red Loading Variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedLoading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedLoading.java
@@ -2,13 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Auton Blue Loading", group = "Autons")
-public class Autonblueloading extends AutonBase {
+@Autonomous(name = "Auton Red Loading", group = "Autons")
+public class AutonRedLoading extends AutonBase {
 
     @Override
     public void runOpMode() {
+        RobotHardwareMap theHardwareMap = new RobotHardwareMap(this.hardwareMap, this);
+        theHardwareMap.initialize();
 
+        waitForStart();
 
+imuDrive(.5,15,0);
 
     }
 }
