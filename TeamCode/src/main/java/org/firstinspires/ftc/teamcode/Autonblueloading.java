6c5de0f432713f164c888fb5074521e012596de8
THEGOOODERDER <Aaron.Doolittle@stu.lok12.org>
2025-10-25 12:02:00 -0400

    feat: added auton blue loading code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonblueloading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonblueloading.java
@@ -2,30 +2,26 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Auton Sample", group = "Autons")
+@Autonomous(name = "Auton Blue Loading", group = "Autons")
 public class Autonblueloading extends AutonBase {
+double imuSpeed = 0.5;
 
     @Override
     public void runOpMode() {
-
-
         initialize();
         waitForStart();
-        //start with only rear right corner touching wall
-        //aim at goal from smaller shooting zone
-        //activate outake before shooting
-        encoderStrafe(.5,-5,3);
-        imuDrive(.5,-3,0);
-        //shoot
-        //activate intake
-        //get artifacts
-        imuDrive(.75,12,0);
-        imuTurn(.5,-135);
-        //encoderStrafe(.5,-10,5);
-        //collect artifacts
-        encoderStrafe(.75,-23,9);
-        imuTurn(.5,-135);
-        //shoot
 
+    //shoot preloaded artifacts
+        imuDrive(imuSpeed,4,0);
+        imuTurn(imuSpeed,18);
+        imuDrive(imuSpeed, 2, 0);
+       imuTurn(imuSpeed,-90);
+        imuDrive(imuSpeed, -37, 0);
+        // active intake
+        imuDrive(imuSpeed, 37, 0);
+        imuTurn(imuSpeed,90);
+        imuDrive(imuSpeed, -2,0);
+        imuTurn(imuSpeed,-18);
+        imuDrive(imuSpeed,-4,0);
     }
 }
