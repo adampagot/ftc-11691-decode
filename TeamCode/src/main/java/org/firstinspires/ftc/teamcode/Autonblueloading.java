58501873523e96e67f6e2afb0366b78a6cf2a095
chronicleglados <thereallferrari@gmail.com>
2025-11-01 13:30:32 -0400

    made more auton paths, perfected red and blue depot and added rough draft for blue and red loading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonblueloading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonblueloading.java
@@ -9,37 +9,51 @@ public class Autonblueloading extends AutonBase {
     @Override
     public void runOpMode() {
         initialize();
-        waitForStart();
 
+        waitForStart();
+         //align with goal
         imuTurn(imuSpeed,15);
+
         //score preloaded artifacts
         sleep(3000);
+
+        //go to get more
         imuTurn(.6,30);
         imuDrive(imuSpeed,-23,0);
         imuTurn(imuSpeed,90);
         imuDrive(imuSpeed,35,0);
+
         //intake on
         sleep(1000);
         imuDrive(imuSpeed,-35,0);
         //intake off
+
+        //go to score
         imuTurn(imuSpeed,90);
         imuDrive(imuSpeed,50,0);
         imuTurn(imuSpeed,135);
+
         //score artifacts
         sleep(3000);
         imuTurn(imuSpeed,45);
         imuDrive(imuSpeed,26,0);
         imuTurn(imuSpeed,90);
         imuDrive(imuSpeed,33,0);
+
         //intake on
         sleep(1000);
         //intake off
+
+        //go to score
         imuDrive(imuSpeed,-33,0);
         imuTurn(imuSpeed,90);
         imuDrive(imuSpeed,26,0);
         imuTurn(imuSpeed,135);
+
         //score artifacts
         sleep(3000);
+
+        //drive outside launch line for rp
         imuTurn(imuSpeed,45);
         imuDrive(imuSpeed,30,0);
     }
