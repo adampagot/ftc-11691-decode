8ef110c579fb564d25eb6fe57d935e5c3941fc9e
ftc <scripps-ftc@no-reply.net>
2025-11-06 20:43:34 -0500

    Improve spaceing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Outtake.java
@@ -83,27 +83,27 @@ if (speed>=1)
             speed = 0.05;
         }
     }
-        public void RunSideTransferServo () {
-            robotHardwareMap.LeftTransferServo.setDirection(CRServo.Direction.FORWARD);
-            robotHardwareMap.RightTransferServo.setDirection(CRServo.Direction.FORWARD);
+    public void RunSideTransferServo () {
+        robotHardwareMap.LeftTransferServo.setDirection(CRServo.Direction.FORWARD);
+        robotHardwareMap.RightTransferServo.setDirection(CRServo.Direction.FORWARD);
 
-            robotHardwareMap.LeftTransferServo.setPower(-1);
-            robotHardwareMap.RightTransferServo.setPower(1);
-        }
+        robotHardwareMap.LeftTransferServo.setPower(-1);
+        robotHardwareMap.RightTransferServo.setPower(1);
+    }
 
-        public void StopSideTransferServo () {
-            robotHardwareMap.LeftTransferServo.setPower(0);
-            robotHardwareMap.RightTransferServo.setPower(0);
-        }
+    public void StopSideTransferServo () {
+        robotHardwareMap.LeftTransferServo.setPower(0);
+        robotHardwareMap.RightTransferServo.setPower(0);
+    }
 
-        public void RunCenterTransferServer (){
-            robotHardwareMap.CenterTransferServo.setDirection(CRServo.Direction.FORWARD);
-            robotHardwareMap.CenterTransferServo.setPower(1);
-        }
+    public void RunCenterTransferServer (){
+        robotHardwareMap.CenterTransferServo.setDirection(CRServo.Direction.FORWARD);
+        robotHardwareMap.CenterTransferServo.setPower(1);
+    }
 
-        public void StopCenterTransferServo () {
-            robotHardwareMap.CenterTransferServo.setPower(0);
-        }
+    public void StopCenterTransferServo () {
+        robotHardwareMap.CenterTransferServo.setPower(0);
+    }
 
 }
 
