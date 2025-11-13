a21bfd41fa3d30c8be333d30d42f13b1f74def71
ftc <scripps-ftc@no-reply.net>
2025-11-12 19:08:44 -0500

    stop motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
@@ -22,6 +22,7 @@ public class Intake {
     }
     public void initialize(){
        robotHardwareMap.intakeMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
+       robotHardwareMap.intakeMotorFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
      }
 
 
